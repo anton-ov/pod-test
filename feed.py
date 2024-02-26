3318ef49dd40664706b071dfec55bc0e702ddd53
anton-ov <142539382+anton-ov@users.noreply.github.com>
2024-02-26 03:40:22 +0000

    create feed.py file

---
+++ feed.py
@@ -0,0 +1,8 @@
+import yaml
+import xml.etree.ElementTree as xml_tree
+
+with open('feed.yaml', 'r') as file:
+    yaml_data = yaml.safe_load(file)
+
+    rss_element - xml_tree.ElementTree('rss')
+    
